48233e547e0187e5fd9b3d176c29869fb272c9bf
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-19 18:11:52 +0300

    Create GlobalKey

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -9,105 +9,112 @@ class RegisterPage extends StatelessWidget {
 
   String? email;
   String? password;
-
+GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 70,
-              ),
-              Image.asset('assets/images/scholar.png'),
-              const Text(
-                'Scholar Chat',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontFamily: 'Pacifico-Regular.ttf',
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 70,
                 ),
-              ),
-              const SizedBox(
-                height: 90,
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Register',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
+                Image.asset('assets/images/scholar.png'),
+                const Text(
+                  'Scholar Chat',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontFamily: 'Pacifico-Regular.ttf',
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  email = data;
-                },
-                hintText: 'Email',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  password = data;
-                },
-                hintText: 'Password',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomButton(
-                onTap: () async {
-                  try {
-                    await registerUser();
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      showSnackBar(context, 'weak-password');
-                    } else if (e.code == 'email-already-in-use') {
-                      showSnackBar(context, 'email-already-in-use');
-                    }
-                  }catch (e)
-                  {
-                    showSnackBar(context, 'there was an error');
-                  }
-                  showSnackBar(context, 'Success');
-                },
-                text: 'Register',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "already have an acount?",
-                    style: TextStyle(
-                      color: Colors.white,
+                ),
+                const SizedBox(
+                  height: 90,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      ' Login',
-                      style: TextStyle(color: Color(0xffC7EDE6)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'Email',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'Password',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(
+                  onTap: () async {
+                    if (formKey.currentState!.validate()) {
+  try {
+    await registerUser();
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      showSnackBar(context, 'weak-password');
+    } else if (e.code == 'email-already-in-use') {
+      showSnackBar(context, 'email-already-in-use');
+    }
+  }catch (e)
+  {
+    showSnackBar(context, 'there was an error');
+  }
+  showSnackBar(context, 'Success');
+}else{
+  
+}
+                  },
+                  text: 'Register',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "already have an acount?",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        ' Login',
+                        style: TextStyle(color: Color(0xffC7EDE6)),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,26 +1,29 @@
-
-
-
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  CustomTextField({this.onChanged,this.hintText});
+  CustomTextField({this.onChanged, this.hintText});
 
-Function(String)? onChanged;
+  Function(String)? onChanged;
 
   String? hintText;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (data) {
+        if (data!.isEmpty ) {
+          return 'field is requerd';
+        }
+      },
       onChanged: onChanged,
       decoration: InputDecoration(
         hintText: hintText,
         hintStyle: const TextStyle(color: Colors.white),
-        focusedBorder:
-         const   OutlineInputBorder(borderSide: BorderSide(color: Colors.blue)),
-        enabledBorder:
-        const    OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-        border:const OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue)),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
+        border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
       ),
     );
   }
