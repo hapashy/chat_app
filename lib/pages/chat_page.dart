f6361b28b9be7477d0a25c44f7d8e363d6f4ae65
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-20 15:51:03 +0300

    add future builder

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -12,59 +12,70 @@ class ChatPage extends StatelessWidget {
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 50,
-            ),
-            const Text(
-              'Chat',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(itemBuilder: (context, index) {
-              return ChatBuble();
-            }),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add({'message': data});
-                controller.clear();
-              },
-              decoration: InputDecoration(
-                hintText: 'Send Message',
-                suffixIcon: Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
+    return FutureBuilder<QuerySnapshot>(
+        future: messages.get(),
+        builder: (context, snapshot) {
+          
+
+          if (snapshot.hasData) {
+            print(snapshot.data!.docs[0]['message']);
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor: kPrimaryColor,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      kLogo,
+                      height: 50,
+                    ),
+                    const Text(
+                      'Chat',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(
-                      color: kPrimaryColor,
-                    )),
+                centerTitle: true,
               ),
-            ),
-          ),
-        ],
-      ),
-    );
+              body: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(itemBuilder: (context, index) {
+                      return ChatBuble();
+                    }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextField(
+                      controller: controller,
+                      onSubmitted: (data) {
+                        messages.add({'message': data});
+                        controller.clear();
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Send Message',
+                        suffixIcon: Icon(
+                          Icons.send,
+                          color: kPrimaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color: kPrimaryColor,
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Text('Loading....');
+          }
+        });
   }
 }
